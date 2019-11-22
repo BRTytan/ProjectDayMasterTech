b648369589cd578dfb9bfc33e5a96c17c43629b7
BRTytan <fe-santos00@hotmail.com>
2019-11-22 12:47:08 -0300

    subindo repositorio com requirements atualizado

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\meu hdd\downloads\estudos\mastertech\projectdaymastertech\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
